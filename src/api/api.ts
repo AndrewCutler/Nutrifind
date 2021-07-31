5b4335cf986beb0475e41ff186a5acf1e4e97a9d
Andrew Cutler <andrewkcutler@gmail.com>
2021-07-31 12:28:07 -0400

    set up api calls and populate dropdown

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,11 @@
-export const search = (term: string) => {
+import axios, { AxiosResponse } from 'axios';
 
+const baseUrl = (term: string): string => `https://api.nal.usda.gov/fdc/v1/foods/search?query=${term}&pageSize=10&api_key=`
+const key = 'r009ldhSE0vUCHfXM2bgGQNIekWLWFdkSr0VipuE'
+
+export const search = (term: string): Promise<AxiosResponse> => {
+    // todo: https://github.com/slorber/awesome-debounce-promise
+    return axios.get(baseUrl(term) + key).then((response: AxiosResponse) => {
+        return response;
+    })
 };
--- src/food-search/FoodSearch.tsx
+++ src/food-search/FoodSearch.tsx
@@ -1,12 +1,34 @@
 import { TextField } from '@material-ui/core';
-import React from 'react';
+import Autocomplete, {
+	AutocompleteRenderInputParams
+} from '@material-ui/lab/Autocomplete';
+import React, { ChangeEvent, useState } from 'react';
+import { search } from '../api/api';
 
 const FoodSearch = (): React.ReactElement => {
-	const handleChange = ({ target: { value } }: any): void => {
+	const [options, setOptions] = useState([]);
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	const handleChange = (_event: ChangeEvent<{}>, value: string): void => {
 		console.log(value);
+		if (value?.trim().length > 2) {
+			search(value.toLowerCase()).then((response) => {
+				console.log('response:', response);
+				setOptions(response.data.foods);
+			});
+		}
 	};
 
-	return <TextField onChange={handleChange} />;
+	// return <TextField onChange={handleChange} />;
+	return (
+		<Autocomplete
+			options={options}
+			onInputChange={handleChange}
+			getOptionLabel={(option: any) => option.description}
+			renderInput={(params: AutocompleteRenderInputParams) => (
+				<TextField {...params} />
+			)}
+		/>
+	);
 };
 
 export default FoodSearch;
