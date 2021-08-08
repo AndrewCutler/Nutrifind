6339d808a6517ae22b4a5bda917b183e006b3269
Andrew Cutler <andrewkcutler@gmail.com>
2021-08-08 14:48:48 -0400

    build out table cells

--- src/food-table/FoodTable.tsx
+++ src/food-table/FoodTable.tsx
@@ -7,10 +7,25 @@ import {
 } from '@material-ui/core';
 import React, { ReactElement } from 'react';
 import { useSelector } from 'react-redux';
+import { Nutrient } from '../models/models';
 import { GeneralState } from '../store/slice';
 
 const FoodTable = (): ReactElement => {
 	const { foods, selectedNutrients } = useSelector(GeneralState);
+	const getFoodNutrientCellContent = (
+		foodNutrients: Nutrient[],
+		nutrientId: number
+	): string => {
+		const nutrient = foodNutrients.find(
+			(foodNutrient: Nutrient) => foodNutrient.nutrientId === nutrientId
+		);
+
+		if (nutrient) {
+			return `${nutrient.value} ${nutrient.unitName}`;
+		}
+
+		return 'No data';
+	};
 
 	return (
 		<>
@@ -32,6 +47,25 @@ const FoodTable = (): ReactElement => {
 									<TableCell>No nutrients selected</TableCell>
 								)}
 							</TableRow>
+							{foods?.map((f) => {
+								return (
+									<TableRow key={f.fdcId}>
+										<TableCell>{f.description}</TableCell>
+										{selectedNutrients?.map((n) => {
+											return (
+												<TableCell
+													key={`${f.fdcId}-${n.nutrientId}`}
+												>
+													{getFoodNutrientCellContent(
+														f.foodNutrients,
+														n.nutrientId
+													)}
+												</TableCell>
+											);
+										})}
+									</TableRow>
+								);
+							})}
 						</TableBody>
 					</Table>
 				</TableContainer>
