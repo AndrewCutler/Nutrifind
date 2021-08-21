f0102caa24e165c164034b322e46f582f7bba9bb
Andrew Cutler <andrewkcutler@gmail.com>
2021-08-21 17:50:39 -0400

    share popover improvements

--- src/share-popover/SharePopover.tsx
+++ src/share-popover/SharePopover.tsx
@@ -1,27 +1,46 @@
-import { Popover, Typography, useTheme } from '@material-ui/core';
+import {
+	Popover,
+	Input,
+	Typography,
+	useTheme,
+	Tooltip
+} from '@material-ui/core';
 import React, { ReactElement } from 'react';
 import { useSelector } from 'react-redux';
 import { generateUrl } from '../api/api';
 import ShareIcon from '@material-ui/icons/Share';
 import { GeneralState } from '../store/slice';
 import { popoverStyles } from '../utility/constants';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
 
 const SharePopover = (): ReactElement => {
 	const theme = useTheme();
 	const { foods, selectedNutrients } = useSelector(GeneralState);
 	const classes = popoverStyles();
 
+	const [url, setUrl] = React.useState<string>('');
 	const [anchorEl, setAnchorEl] = React.useState(null);
+	const [tooltipOpen, setTooltipOpen] = React.useState<boolean>(false);
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const handleClick = ({ currentTarget }: any): void => {
-		const url = generateUrl(foods, selectedNutrients);
-		navigator.clipboard.writeText(url);
+	const handleShareClick = ({ currentTarget }: any): void => {
+		// navigator.clipboard.writeText(url);
+		setUrl(generateUrl(foods, selectedNutrients));
 		setAnchorEl(currentTarget);
+	};
+
+	const handleCopyClick = (): void => {
+		navigator.clipboard.writeText(url);
+		setTooltipOpen(true);
 	};
 
 	const handleClose = (): void => {
 		setAnchorEl(null);
+		handleTooltipClose();
+	};
+
+	const handleTooltipClose = () => {
+		setTooltipOpen(false);
 	};
 
 	const open = Boolean(anchorEl);
@@ -33,7 +52,7 @@ const SharePopover = (): ReactElement => {
 					cursor: 'pointer',
 					marginRight: theme.spacing(2)
 				}}
-				onClick={handleClick}
+				onClick={handleShareClick}
 			/>
 			<Popover
 				open={open}
@@ -48,9 +67,23 @@ const SharePopover = (): ReactElement => {
 					horizontal: 'center'
 				}}
 			>
-				<Typography className={classes.root}>
-					Shareable link copied to the clipboard.
-				</Typography>
+				<div className={classes.root}>
+					<Input value={url} />
+					<Tooltip
+						style={{ marginLeft: 8, cursor: 'pointer' }}
+						title='Link copied'
+						disableFocusListener
+						disableHoverListener
+						disableTouchListener
+						open={tooltipOpen}
+						onClose={handleTooltipClose}
+					>
+						<FileCopyIcon
+							fontSize='small'
+							onClick={handleCopyClick}
+						/>
+					</Tooltip>
+				</div>
 			</Popover>
 		</>
 	);
